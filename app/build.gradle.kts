3b218e1f038166bcd7f96363eb58ef6e3f424596
Leonan Marques <leonan.marques886@gmail.com>
2024-03-28 20:41:55 -0300

    firebase dependencies have been added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,34 +43,37 @@ android {
 
 dependencies {
   
+  // Compose dependencies
   val composeBom = platform("androidx.compose:compose-bom:2024.03.00")
   implementation(composeBom)
   androidTestImplementation(composeBom)
-  
   implementation("androidx.compose.material3:material3")
-  
   implementation("androidx.compose.ui:ui-tooling-preview")
   debugImplementation("androidx.compose.ui:ui-tooling")
-  
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
-  
   implementation("androidx.compose.material:material-icons-core")
   implementation("androidx.compose.material:material-icons-extended")
   implementation("androidx.compose.material3:material3-window-size-class")
-  
   implementation("androidx.activity:activity-compose:1.8.2")
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
   implementation("androidx.compose.runtime:runtime-livedata")
   implementation("androidx.compose.runtime:runtime-rxjava2")
-  
-  
+
+// AndroidX dependencies
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("com.google.android.material:material:1.11.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+// Material Design dependencies
+  implementation("com.google.android.material:material:1.11.0")
+
+// Firebase dependencies
   implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
   implementation("com.google.firebase:firebase-analytics")
+  implementation("com.google.firebase:firebase-auth-ktx")
+
+// Testing dependencies
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
