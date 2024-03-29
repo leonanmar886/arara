bc32b718542a6c5ac20450887b60d76363b06721
Leonan Marques <leonan.marques886@gmail.com>
2024-03-28 22:12:57 -0300

    navigation dependency have been added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,4 +77,12 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  
+// Navigation
+  val nav_version = "2.7.7"
+  implementation("androidx.navigation:navigation-compose:$nav_version")
+  implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+  implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+  
+  implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
