63c915120dc00ff8ceb40374eaf3e7955b8bb3b4
leonan-gotest <leonan.carneiro@gotest.com.br>
2024-04-22 20:43:13 -0300

    input field styles have been updated

--- app/src/main/java/com/example/arara/ui/components/StyledInputField.kt
+++ app/src/main/java/com/example/arara/ui/components/StyledInputField.kt
@@ -2,14 +2,17 @@ package com.example.arara.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.VisualTransformation
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputField(
   value: String,
@@ -29,6 +32,13 @@ fun InputField(
       keyboardOptions = keyboardOptions,
       isError = errorMessage.isNotEmpty(),
       singleLine = true,
+      shape = MaterialTheme.shapes.small,
+      colors = TextFieldDefaults.colors(
+        cursorColor = Color.Black,
+        disabledLabelColor = Color.LightGray,
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent
+      ),
       modifier = modifier
     )
     if(errorMessage.isNotEmpty()) {
