208ba4d10b0465bf22c5cf73395c20935de5c5e6
CH <carloshenrriky1@gmail.com>
2024-04-25 16:31:32 -0300

    foi alterado o tamanho da logo na tela de login

--- app/src/main/java/com/example/arara/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/arara/ui/screens/login/LoginScreen.kt
@@ -8,11 +8,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -80,6 +81,9 @@ fun LoginContent(
       Image(
         painter = painterResource(id = R.drawable.logo),
         contentDescription = "Logo",
+        modifier = Modifier
+          .size(width = 100.dp, height = 160.dp)
+          .fillMaxWidth()
       )
       Text(
         text = "Minha Arara",
