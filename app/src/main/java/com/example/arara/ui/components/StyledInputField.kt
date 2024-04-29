27283c42765a6a92f37bfb81c2127b47ebea437a
leonan-gotest <leonan.carneiro@gotest.com.br>
2024-04-29 18:50:48 -0300

    imports inuteis excluídos

--- app/src/main/java/com/example/arara/ui/components/StyledInputField.kt
+++ app/src/main/java/com/example/arara/ui/components/StyledInputField.kt
@@ -2,13 +2,13 @@ package com.example.arara.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -17,11 +17,11 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.sp
 import com.example.arara.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputField(
   value: String,
   onValueChange: (String) -> Unit,
+  onClick: () -> Unit = {},
   label: String,
   visualTransformation: VisualTransformation? = null,
   keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
@@ -52,7 +52,7 @@ fun InputField(
         unfocusedIndicatorColor = Color.Transparent,
         unfocusedContainerColor = Color(0xFFCBCBCB).copy(alpha = "7A".toInt(16) / 255f)
       ),
-      modifier = modifier
+      modifier = modifier.onFocusEvent {if (it.isFocused) onClick() }
     )
     if(errorMessage.isNotEmpty()) {
       Text(
--- app/src/main/java/com/example/arara/ui/screens/user/register/UserRegisterScreen.kt
+++ app/src/main/java/com/example/arara/ui/screens/user/register/UserRegisterScreen.kt
@@ -1,6 +1,5 @@
 package com.example.arara.ui.screens.user.register
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,13 +8,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -23,19 +28,49 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.arara.ui.navigation.NavigationDestination
-
 import com.example.arara.R
 import com.example.arara.ui.components.InputField
+import com.example.arara.ui.navigation.NavigationDestination
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 object UserRegisterDestination: NavigationDestination {
   override val route = "user_register"
   override val titleRes = R.string.user_register_title
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserRegisterContent() {
   
+  val focusManager = LocalFocusManager.current
+  var showDatePickerDialog by remember {
+    mutableStateOf(false)
+  }
+  val datePickerState = rememberDatePickerState()
+  var selectedDate by remember {
+    mutableStateOf("")
+  }
+  if (showDatePickerDialog) {
+    DatePickerDialog(
+      onDismissRequest = { showDatePickerDialog = false },
+      confirmButton = {
+        Button(
+          onClick = {
+            datePickerState
+              .selectedDateMillis?.let { millis ->
+                selectedDate = millis.toBrazilianDateFormat()
+              }
+            showDatePickerDialog = false
+          }) {
+          Text(text = "Escolher data")
+        }
+      }) {
+      DatePicker(state = datePickerState)
+    }
+  }
   
   Box(
     modifier = Modifier.padding(16.dp)
@@ -112,11 +147,15 @@ fun UserRegisterContent() {
         )
         
         InputField(
-          value = "",
+          value = selectedDate,
           onValueChange = {},
           label = "Data de Nascimento",
           errorMessage = "",
-          modifier = Modifier.fillMaxWidth()
+          modifier = Modifier.fillMaxWidth(),
+          onClick = {
+            showDatePickerDialog = true
+            focusManager.clearFocus(force = true)
+          },
         )
         
         InputField(
@@ -148,4 +187,16 @@ fun UserRegisterContent() {
       }
     }
   }
+}
+
+fun Long.toBrazilianDateFormat(
+  pattern: String = "dd/MM/yyyy"
+): String {
+  val date = Date(this)
+  val formatter = SimpleDateFormat(
+    pattern, Locale("pt-br")
+  ).apply {
+    timeZone = TimeZone.getTimeZone("GMT")
+  }
+  return formatter.format(date)
 }
