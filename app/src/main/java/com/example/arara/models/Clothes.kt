730d3c49a087468f37878ddb811deb5b6eae222d
Victoria de Castro <victoriacastro3108@gmail.com>
2024-05-30 17:25:16 -0300

    Arquivos de Clothes adicionados

---
+++ app/src/main/java/com/example/arara/models/Clothes.kt
@@ -0,0 +1,9 @@
+package com.example.arara.models
+
+data class Clothes (
+    val name: String,
+    val color: String,
+    val size: String,
+    val description: String,
+    val tags: List<String>
+)
---
+++ app/src/main/java/com/example/arara/ui/screens/clothes/ClothesDetailsScreen.kt
@@ -0,0 +1,2 @@
+package com.example.arara.ui.screens.clothes
+
