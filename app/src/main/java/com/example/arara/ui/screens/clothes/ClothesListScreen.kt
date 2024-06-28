27d04d7af9ffa328d13c7771bc4c61c066cc14ee
Leonan Marques <leonan.marques886@gmail.com>
2024-06-28 14:54:44 -0300

    componente de busca baseada em opções foi implementada

--- app/src/main/java/com/example/arara/ui/screens/clothes/ClothesListScreen.kt
+++ app/src/main/java/com/example/arara/ui/screens/clothes/ClothesListScreen.kt
@@ -1,10 +1,14 @@
 package com.example.arara.ui.screens.clothes
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,6 +27,10 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -37,6 +45,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -222,6 +232,7 @@ fun ImageCard(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchField(
     value: String,
@@ -232,41 +243,71 @@ fun SearchField(
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     errorMessage: String,
     multiline: Boolean? = false,
+    options: List<String> = listOf("Tamanho", "Cor", "Estilo", "Estação"),
     modifier: Modifier
 ) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOption by remember { mutableStateOf("Tamanho") }
+    
+    val filteredOptions = remember(value) {
+        options.filter { it.contains(value, ignoreCase = true) }
+    }
     
     Column {
-        OutlinedTextField(
-            value = value,
-            onValueChange = onValueChange,
-            visualTransformation =  visualTransformation ?: VisualTransformation.None,
-            keyboardOptions = keyboardOptions,
-            isError = errorMessage.isNotEmpty(),
-            singleLine = multiline == false || multiline == null,
-            label = {
-                if (label != null) {
-                    Text(
-                        text = label,
-                        fontFamily = FontFamily(Font(R.font.quicksand)),
-                        fontWeight = FontWeight.W700,
-                        fontSize = 12.sp
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+            modifier = modifier.width(200.dp).background(Color.Transparent)
+        ) {
+            OutlinedTextField(
+                value = value,
+                onValueChange = {
+                    onValueChange(it)
+                    expanded = true
+                },
+                visualTransformation = visualTransformation ?: VisualTransformation.None,
+                keyboardOptions = keyboardOptions,
+                isError = errorMessage.isNotEmpty(),
+                singleLine = multiline == false || multiline == null,
+                label = {
+                    if (label != null) {
+                        Text(
+                            text = label,
+                            fontFamily = FontFamily(Font(R.font.quicksand)),
+                            fontWeight = FontWeight.W700,
+                            fontSize = 12.sp
+                        )
+                    }
+                },
+                shape = MaterialTheme.shapes.medium,
+                colors = OutlinedTextFieldDefaults.colors(
+                    cursorColor = Color.Black,
+                    focusedBorderColor = Color.Black,
+                    focusedLabelColor = Color.Black,
+                ),
+                textStyle = MaterialTheme.typography.labelSmall,
+                trailingIcon = aboutIcon?.let { { aboutIcon() } },
+                modifier = Modifier.menuAnchor()
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .background(Color.White)
+            ) {
+                filteredOptions.forEach { option ->
+                    DropdownMenuItem(
+                        onClick = {
+                            selectedOption = option
+                            expanded = false
+                            onValueChange(option)
+                        },
+                        text = { Text(text = option, style = MaterialTheme.typography.labelMedium) },
                     )
                 }
-            },
-            shape = MaterialTheme.shapes.medium,
-            colors = OutlinedTextFieldDefaults.colors(
-                cursorColor = Color.Black,
-                disabledLabelColor = Color.LightGray,
-                focusedBorderColor = Color.Black,
-                focusedLabelColor = Color.Black,
-            ),
-            textStyle = MaterialTheme.typography.labelSmall,
-            trailingIcon = aboutIcon?.let { { aboutIcon() } },
-            modifier = modifier
-                .width(200.dp)
-            
-        )
-        if(errorMessage.isNotEmpty()) {
+            }
+        }
+        if (errorMessage.isNotEmpty()) {
             Text(
                 text = errorMessage,
                 color = Color.Red,
@@ -276,6 +317,7 @@ fun SearchField(
         }
     }
 }
+
 
 @Composable
 fun Footer(
